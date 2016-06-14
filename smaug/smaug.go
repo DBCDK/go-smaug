98e30fd72429670cafe01a6e86909a7437382bf1
Adam Finn Tulinius <atu@dbc.dk>
2016-06-14 15:27:30 +0200

    prioritize access token from query params higher than the one from a cookie

--- smaug/smaug.go
+++ smaug/smaug.go
@@ -9,14 +9,14 @@ import (
 )
 
 func TokenFromRequest(request *http.Request) (*string, error) {
-	token_from_cookie, err := request.Cookie("access_token")
-	if err == nil && token_from_cookie != nil {
-		return &token_from_cookie.Value, nil
-	}
-
 	token_from_query := request.URL.Query().Get("access_token")
 	if len(token_from_query) > 0 {
 		return &token_from_query, nil
+	}
+
+	token_from_cookie, err := request.Cookie("access_token")
+	if err == nil && token_from_cookie != nil {
+		return &token_from_cookie.Value, nil
 	}
 
 	return nil, errors.New("no access token")
